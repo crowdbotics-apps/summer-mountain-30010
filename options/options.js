200b55d54a55184e339b9a32906e1605137dac94
Crowdbotics <team@crowdbotics.com>
2021-08-21 20:13:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "summer_mountain_30010Identifier",
+  url: "https://summer_mountain_30010Identifier.botics.co",
+  api: "https://summer_mountain_30010Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
